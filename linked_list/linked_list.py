c4fda9b4454278f092e5c404434294e1b3027b13
michdol <michal.dolbniak@gmail.com>
2019-10-30 19:58:14 +0900

    Created linked list

---
+++ linked_list/linked_list.py
@@ -0,0 +1,115 @@
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.previous = None
+
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    @property
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+    def __contains__(self, item):
+        current = self.head
+        while current:
+            if current.data == item:
+                return True
+            current = current.next
+        return False
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError("Key should be integer")
+        current = self.head
+        current_index = 0
+        while current:
+            if current_index == key:
+                break
+            current = current.next
+            current_index += 1
+        try:
+            return current.data
+        # Run out of nodes
+        except AttributeError:
+            raise IndexError("Index out of range")
+
+    def push(self, item):
+        new_node = Node(data=item)
+        new_node.next = self.head
+        self.head = new_node
+        if new_node.next:
+            new_node.next.previous = new_node
+
+    def delete(self, key):
+        current = self.head
+
+        if current and current.data == key:
+            self.head = current.next
+            if self.head:
+                self.head.previous = None
+            del current
+            return
+
+        while current:
+            if current.data == key:
+                break
+            current = current.next
+
+        if not current:
+            raise ValueError("Key not found")
+
+        previous = current.previous
+        next_ = current.next
+        if previous:
+            previous.next = next_
+        if next_:
+            next_.previous = previous
+        del current
+
+    def delete_node(self, index):
+        current = self.head
+        current_index = 0
+
+        if current and current_index == index:
+            self.head = current.next
+            if self.head:
+                self.head.previous = None
+            return
+
+        while current:
+            if current_index == index:
+                break
+            current = current.next
+            current_index += 1
+
+        if not current:
+            raise IndexError("Index out of range")
+
+        previous = current.previous
+        next_ = current.next
+        if previous:
+            previous.next = next_
+        if next_:
+            next_.previous = previous
+        del current
+
+    def __iter__(self):
+        return self._generator()
+
+    def _generator(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
---
+++ linked_list/tests.py
@@ -0,0 +1,220 @@
+from unittest import main, TestCase
+
+from linked_list import LinkedList, Node
+
+
+class LinkedListTest(TestCase):
+    def _create_ll(self):
+        l = LinkedList()
+        for i in range(1, 6):
+            l.push(i)
+        return l
+
+    def test_size(self):
+        l = self._create_ll()
+        self.assertEqual(l.size, 5)
+
+    def test_push(self):
+        l = LinkedList()
+        self.assertIsNone(l.head)
+        l.push(1)
+        self.assertEqual(l.head.data, 1)
+        self.assertEqual(l.tail.data, 1)
+        self.assertIsNone(l.head.next)
+        self.assertIsNone(l.head.previous)
+        self.assertEqual(l.size, 1)
+
+        l.push(2)
+        self.assertEqual(l.head.data, 2)
+        self.assertEqual(l.tail.data, 1)
+        self.assertEqual(l.head.next, l.tail)
+        self.assertEqual(l.tail.previous, l.head)
+        self.assertIsNone(l.head.previous)
+        self.assertEqual(l.head.next.data, 1)
+        self.assertEqual(l.head.next.previous.data, 2)
+        self.assertIsNone(l.head.next.next)
+        self.assertEqual(l.size, 2)
+
+        """
+        1   - tail
+        2
+        3   - head
+
+        head.next.data = 2
+        tail.previous.data = 2
+        - head.next == tail.previous
+        """
+        l.push(3)
+        self.assertEqual(l.head.data, 3)
+        self.assertEqual(l.tail.data, 1)
+        self.assertEqual(l.tail.previous, l.head.next)
+        self.assertIsNone(l.head.previous)
+        self.assertEqual(l.head.next.data, 2)
+        self.assertEqual(l.head.next.next.data, 1)
+        self.assertEqual(l.head.next.previous.data, 3)
+        self.assertIsNone(l.head.next.next.next)
+        self.assertEqual(l.head.next.next.previous.data, 2)
+        self.assertEqual(l.size, 3)
+
+    def test_delete(self):
+        l = LinkedList()
+        l.push(4)
+        l.push(3)
+        l.push(2)
+        l.push(1)
+
+        l.delete(2)
+
+        self.assertEqual(l.head.data, 1)
+        self.assertEqual(l.head.next.data, 3)
+        self.assertEqual(l.head.next.previous.data, 1)
+
+    def test_delete_head(self):
+        l = LinkedList()
+        l.push(3)
+        l.push(2)
+        l.push(1)
+        self.assertEqual(l.head.data, 1)
+
+        l.delete(1)
+
+        self.assertEqual(l.head.data, 2)
+        self.assertEqual(l.head.next.data, 3)
+        self.assertIsNone(l.head.previous)
+
+    def test_delete_key_not_present(self):
+        l = LinkedList()
+        l.push(1)
+        l.push(2)
+        l.push(3)
+
+        with self.assertRaises(ValueError):
+            l.delete(4)
+
+        l = LinkedList()
+        with self.assertRaises(ValueError):
+            l.delete(1)
+
+    def test_delete_from_single_item_list(self):
+        l = LinkedList()
+        l.push(1)
+
+        l.delete(1)
+
+        self.assertEqual(l.size, 0)
+        self.assertIsNone(l.head)
+
+    def test_delete_node(self):
+        l = LinkedList()
+        l.push(2)
+        l.push(1)
+
+        l.delete_node(0)
+
+        self.assertEqual(l.size, 1)
+        self.assertEqual(l.head.data, 2)
+        self.assertIsNone(l.head.previous)
+
+    def test_delete_node_empty_list(self):
+        l = LinkedList()
+
+        with self.assertRaises(IndexError):
+            l.delete_node(0)
+
+    def test_delete_node_single_item_list(self):
+        l = LinkedList()
+        l.push(1)
+
+        l.delete_node(0)
+
+        self.assertEqual(l.size, 0)
+        self.assertIsNone(l.head)
+
+    def test_delete_node_middle_item(self):
+        l = LinkedList()
+        l.push(4)
+        l.push(3)
+        l.push(2)
+        l.push(1)
+
+        l.delete_node(1)
+
+        self.assertEqual(l.size, 3)
+        self.assertEqual(l.head.next.data, 3)
+        self.assertEqual(l.head.next.previous.data, 1)
+
+    def test_delete_node_last_item(self):
+        l = LinkedList()
+        l.push(3)
+        l.push(2)
+        l.push(1)
+
+        l.delete_node(2)
+
+        self.assertEqual(l.size, 2)
+        self.assertEqual(l.head.next.data, 2)
+        self.assertIsNone(l.head.next.next)
+
+    def test_item_in_list(self):
+        l = LinkedList()
+
+        self.assertFalse(1 in l)
+
+        l.push(1)
+
+        self.assertFalse(2 in l)
+        self.assertTrue(1 in l)
+
+    def test_get_by_index(self):
+        l = LinkedList()
+        l.push(1)
+
+        ret = l[0]
+        self.assertEqual(ret, 1)
+
+    def test_get_by_index_empty_list(self):
+        l = LinkedList()
+
+        with self.assertRaises(IndexError):
+            l[0]
+        l.push(1)
+        with self.assertRaises(IndexError):
+            l[1]
+
+    def test_get_by_index_more_tests(self):
+        l = LinkedList()
+        l.push(4)
+        l.push(3)
+        l.push(2)
+        l.push(1)
+
+        self.assertEqual(l[3], 4)
+        self.assertEqual(l[2], 3)
+        self.assertEqual(l[1], 2)
+        self.assertEqual(l[0], 1)
+
+    def test_get_by_negative_index(self):
+        l = LinkedList()
+        l.push(1)
+
+        self.assertEqual(l[-1], 1)
+
+    def test_get_by_index_key_type_error(self):
+        l = LinkedList()
+        l.push(1)
+
+        with self.assertRaises(TypeError):
+            l['0']
+
+    def test_iter(self):
+        l = self._create_ll()
+
+        expected = 5
+        for node in l:
+            self.assertEqual(node.data, expected)
+            expected -= 1
+        self.assertEqual(expected, 0)
+
+
+if __name__ == '__main__':
+    main()
